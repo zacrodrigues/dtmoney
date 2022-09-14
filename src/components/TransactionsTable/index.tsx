14a67dc613922deb0822bcdce14d416d55df1c17
zacrodrigues <rodrigues.contano@gmail.com>
2022-09-14 10:10:28 -0300

    Consumindo API - Configurando cliente do Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-      .then(res => res.json())
-      .then(data => console.log(data))
+    api.get('transactions')
+      .then(res => console.log(res.data))
   }, []);
 
   return(
