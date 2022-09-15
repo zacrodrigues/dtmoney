2db5bd18f69074fc1defacdb20fea7716dcf8513
zacrodrigues <rodrigues.contano@gmail.com>
2022-09-15 14:57:39 -0300

    Modal & Forms - Cores dos botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -41,6 +41,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             type="button"
             onClick={() => { setType('deposit') }}
             isActive={type === 'deposit'}
+            activeColor='green'
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
@@ -50,6 +51,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             type="button"
             onClick={() => { setType('withdraw') }}
             isActive={type === 'withdraw'}
+            activeColor='red'
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -58,14 +58,24 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
-}
+  activeColor: 'green' | 'red';
+};
+
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${(props) => props.isActive ? '#aaa' : 'transparent'};
+  background: ${(props) => props.isActive
+    ? transparentize(0.9, colors[props.activeColor])
+    : 'transparent'
+  };
+
   display: flex;
   align-items: center;
   justify-content: center;
@@ -74,17 +84,17 @@ export const RadioBox = styled.button<RadioBoxProps>`
 
   &:hover {
     border-color: ${darken(0.1, '#d7d7d7')};
-  }
+  };
 
   img {
     width: 20px;
     height: 20px;
-  }
+  };
 
   span {
     display: inline-block;
     margin-left: 1rem;
     font-size: 1rem;
     color: var(--text-title);
-  }
+  };
 `;
